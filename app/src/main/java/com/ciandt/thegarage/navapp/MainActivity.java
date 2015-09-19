712020a09077564ff86ad74f1ff364257ec7d801
Carlos Rodrigues <analista.carlosh@gmail.com>
2015-09-19 16:46:51 -0300

    Removido o uso do TimeRun, deixado somente o scann de beacons e Adicionado tema do Material Design

--- app/src/main/java/com/ciandt/thegarage/navapp/MainActivity.java
+++ app/src/main/java/com/ciandt/thegarage/navapp/MainActivity.java
@@ -32,11 +32,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int NOTIFICATION_ID = 123;
 
     // Y positions are relative to height of bg_distance image.
     private static final double RELATIVE_START_POS = 320.0 / 1110.0;
@@ -51,20 +53,10 @@ public class MainActivity extends AppCompatActivity {
     private int segmentLength = -1;
 
     private int beaconsSize = 0;
-    private TextView restJson;
     private ArrayList<Beacon> beacons;
 
-    public static final String EXTRAS_TARGET_ACTIVITY = "extrasTargetActivity";
-    public static final String EXTRAS_BEACON = "extrasBeacon";
     private static final int REQUEST_ENABLE_BT = 1234;
     private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
-    private static final String UrlService = "http://citbeacons.appspot.com/ws/beacon/findByMacAddress/macAddress=";
-    String beaconMacAddress = "";
-    String ultimoBeaconMacAddress ="";
-    String proximoBeaconNaArea = "";
-    String beaconsAntecessores = "";
-    String mensagemBeacon = "";
-    String mensagemFinalAoUsuario = "";
     private Repository repository;
     private TimerRun timerRun;
 
@@ -72,28 +64,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        restJson = (TextView) findViewById(R.id.textView);
         beaconManager = new BeaconManager(this);
 
         timerRun = new TimerRun(this);
         repository = new Repository(this);
 
+        beaconManager.setForegroundScanPeriod(TimeUnit.SECONDS.toMillis(Constants.PERIOD_MILLIS_SCAN_RANGING), Constants.WAIT_TIME_MILLIS_SCAN_RANGING);
+
+        // CallBack Scan Beacons
         beaconManager.setRangingListener(new BeaconManager.RangingListener() {
             @Override
             public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+
+                        Log.i(TAG, "setRangingListener--onBeaconsDiscovered");
                         JSONObject jsonObj = new JSONObject();
 
                         try {
-                            jsonObj.put("macAddress", beacons.get(0).getMacAddress());
-                            jsonObj.put("rssi", beacons.get(0).getRssi());
+
+                            Log.i(TAG, "setRangingListener--onBeaconsDiscovered::" + beacons.toString());
+
+                            if(beacons != null && beacons.size() > 0){
+                                Toast.makeText(getApplicationContext(), "macAddress:" + beacons.get(0).getMacAddress().toString(), Toast.LENGTH_LONG).show();
+
+                                jsonObj.put("macAddress", beacons.get(0).getMacAddress());
+                                jsonObj.put("rssi", beacons.get(0).getRssi());
+
+                                repository.put(Constants.BEACON_ANTERIOR_KEY, repository.get(Constants.BEACON_ATUAL_KEY));
+                                repository.put(Constants.BEACON_ATUAL_KEY, jsonObj.toString());
+                            }
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        repository.put(Constants.BEACON_ANTERIOR_KEY, repository.get(Constants.BEACON_ATUAL_KEY));
-                        repository.put(Constants.BEACON_ATUAL_KEY, jsonObj.toString());
                     }
                 });
             }
@@ -127,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Verificando se o Device tem Bluetooth
         if (!beaconManager.hasBluetooth()) {
-            Toast.makeText(this, "Device does not have Bluetooth Low Energy", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "O aplicativo não irá funcionar, pois seu Celular não tem Bluetooth.", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -135,26 +140,45 @@ public class MainActivity extends AppCompatActivity {
         if (!beaconManager.isBluetoothEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-
-            Log.i("Main", "startActivityForResult");
-            //return;
         } else {
-            connectToService();
-            timerRun.start();
+            connectToServiceScannBeacon();
+            //timerRun.start();
         }
     }
 
+    @Override protected void onResume(){
+        super.onResume();
+    }
+
     @Override protected void onStop() {
-        try {
+        /*try {
             //Stop busca de Beacon
             beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
         } catch (RemoteException e) {
             Log.d(TAG, "Error while stopping ranging", e);
-        }
+        }*/
         super.onStop();
     }
 
-    private void connectToService() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Verificando acesso a permissão
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode != RESULT_OK) {
+                Toast.makeText(MainActivity.this, "O aplicativo não irá funcionar, pois não foi dada a permissão de habilitar o Bluetooth.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Aceito
+            if (resultCode == RESULT_OK) {
+                connectToServiceScannBeacon();
+                //timerRun.start();
+            }
+        }
+    }
+
+    private void connectToServiceScannBeacon() {
         Collections.<Beacon>emptyList();
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
@@ -167,23 +191,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // Verificando acesso a permissão
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode != RESULT_OK) {
-                Toast.makeText(MainActivity.this, "Não foi dada a permissão de habilitar o Bluetooth", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            // Aceito
-            if (resultCode == RESULT_OK) {
-                connectToService();
-                timerRun.start();
-            }
-        }
     }
 }
